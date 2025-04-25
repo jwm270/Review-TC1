d010f5dd6b8c1631441ac7761f3f65357ee69fc2
Jimmy Moore <jwm270@gmail.com>
2025-04-25 14:33:34 -0500

    copied from the web and not really understood

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace review
+{
+    public class Program
+
+    {
+        public static double GetCircleArea(double radius)
+        {
+            const float PI = 3.14159265358979323846f;
+            double area = PI * radius * radius;
+            return area;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = GetCircleArea(radius);
+            Console.WriteLine("The area of the circle is: {0}", area);
+            Console.ReadKey();
+        }
+    }
+}
